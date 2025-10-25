54b33ba9deecb34222a23a07cb721b75096c8632
Philip O'Donovan <b00177493@mytudublin.ie>
2025-10-25 16:20:41 +0100

    Create working

--- install.php
+++ install.php
@@ -3,6 +3,7 @@
 require "config.php";
 
 try {
+
  $connection = new PDO("mysql:host=$host", $username, $password,
 $options);
  $sql = file_get_contents("data/init.sql");
--- public/create.php
+++ public/create.php
@@ -1,5 +1,32 @@
 <?php include "templates/header.php"; ?>
 <h2>Add a user</h2>
+
+<?php
+if (isset($_POST['submit'])) {
+
+    try {
+        require "../src/DBconnect.php";
+
+        $new_user = array(
+                "firstname" => $_POST['firstname'],
+                "lastname" => $_POST['lastname'],
+                "email" => $_POST['email'],
+                "age" => $_POST['age'],
+                "location" => $_POST['location']
+        );
+        $sql = sprintf("INSERT INTO %s (%s) values (%s)", "users",
+                implode(", ", array_keys($new_user)),
+                ":" . implode(", :", array_keys($new_user)));
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
 <form method="post">
     <label for="firstname">First Name</label>
     <input type="text" name="firstname" id="firstname" required>
@@ -14,4 +41,5 @@
     <input type="submit" name="submit" value="Submit">
 </form>
 <a href="index.php">Back to home</a>
+
 <?php include "templates/footer.php"; ?>
